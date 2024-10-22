4f7eae1f5f1d72a316ec582302326925f2dff8a9
Vadim Chugunov <vadimcn@gmail.com>
2024-10-22 11:41:13 -0700

    Add Zstd to linux-builder

---
+++ linux-builder/docker/stretch/usr/include/zdict.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d19ab287eed353c9623f4a02e2dd98a2caefe2cfb58257d27c7b41d50f07d1a5
+size 18451
---
+++ linux-builder/docker/stretch/usr/include/zstd_errors.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a41712d8fe7b654f1aab53b55cedd137b4bd927aa38773a9469fdc8668cdfa4c
+size 3828
